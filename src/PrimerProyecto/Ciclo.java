dabdce6d1c5b46c7a8578f04ab3e30620901b990
loberasv <l.lobera19@gmail.com>
2025-11-18 12:20:48 +0100

    Primer commit
    Primer commit con la clase ciclo.java y la clase nombre.java

---
+++ src/PrimerProyecto/Ciclo.java
@@ -0,0 +1,12 @@
+package PrimerProyecto;
+
+
+public class Ciclo {
+
+
+	public static void main(String[] args) {
+		System.out.println("Técnico Superior en Desarrollo de Aplicaiones");
+
+	}
+
+}
---
+++ src/PrimerProyecto/Nombre.java
@@ -0,0 +1,8 @@
+package PrimerProyecto;
+
+public class Nombre {
+	public static void main(String[] args) {
+		System.out.println("Lucas Lobera Muñoz");
+	}
+
+}
